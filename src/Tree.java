71f0c8c22ca7a7806949266a9c2dcf052321dd51
SokhnaAnta <sokhnaantambacke@esp.sn>
2023-07-08 19:24:12 +0000

    introduction du parametre /F

--- src/Tree.java
+++ src/Tree.java
@@ -7,22 +7,41 @@ public class Tree{
       public static void main(String[] args) {
 
             String path = Paths.get(".").toAbsolutePath().toString(); 
+            boolean showfile = false ; 
+            if(args.length==1)
 
-            if(args.length>0)
-            {
-                  path = args[0] ;
-            }      
+            {  
+                  if (!args[0].equalsIgnoreCase("/f")){
+                      path = args[0];
+                  }
+                  else showfile=true ;
+                                   
+            }     
+            if(args.length ==2){
+                   if(args[1].equalsIgnoreCase("/f")) showfile = true;
+                    path = args[0];
 
+            } 
+            
             File file = new File(path) ;
              
             if (file.isDirectory()) {
-                  Directory mainDirectory = new Directory(file.getName(), 0);
-                  subDirectory(file, mainDirectory);
+                  Directory mainDirectory = new Directory(path, 0);
+                  if(showfile){
+                        subDirectoryAndFile(file, mainDirectory);
+                  }
+                  else{
+                        subDirectory(file, mainDirectory);
+                  }
+                 
                   mainDirectory.afficher();
             } 
+            else{
+                  System.out.println("le chemin specifié n'est pas un folder");
+            }
       }
 
-      private static  void subDirectory(File directory, Directory parent) 
+      private static  void subDirectoryAndFile(File directory, Directory parent) 
       {
             File[] files = directory.listFiles();
             
@@ -31,11 +50,26 @@ public class Tree{
                   if (file.isDirectory()) {
                         Directory subDirectory = new Directory(file.getName(), parent.getColonne() + 1);
                         parent.ajout(subDirectory);
-                        subDirectory(file, subDirectory);
+                        subDirectoryAndFile(file, subDirectory);
                   } else {
                         Fichier fichier = new Fichier(file.getName(), parent.getColonne() + 1);
                         parent.ajout(fichier);
                   }
+            }
+            }
+      }
+
+      private static void subDirectory(File directory, Directory parent){
+         
+            File[] files = directory.listFiles();
+            
+            if (files != null) {
+            for (File file : files) {
+                  if (file.isDirectory()) {
+                        Directory subDirectory = new Directory(file.getName(), parent.getColonne() + 1);
+                        parent.ajout(subDirectory);
+                        subDirectory(file, subDirectory);
+                  } 
             }
             }
       }
